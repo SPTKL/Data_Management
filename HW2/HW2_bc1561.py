a3cdcdf0b750b6a526f0f3ddfc2249dc8b88ca1c
sptkl <caobaiyue@gmail.com>
2018-07-11 22:43:06 -0400

    add hw2

---
+++ HW2/HW2_bc1561.py
@@ -0,0 +1,29 @@
+from pyspark import SparkContext
+import csv
+import sys
+
+def cuisine_camis(pid, record):
+    import csv
+    if pid==0:
+        next(record)
+    reader = csv.reader(record)
+    for row in reader:
+        camis = row[0]
+        cuisine = row[7]
+        yield (camis, cuisine)
+
+if __name__=='__main__':
+    sc = SparkContext()
+
+    NYC_REST = sys.argv[1]
+
+    nyc_rest = sc.textFile(NYC_RESTAURANT, use_unicode=False).cache()
+
+    nyc_rest_records = sat.mapPartitionsWithIndex(nyc_rest)
+
+    nyc_rest_records.mapPartitionsWithIndex(cuisine_camis) \
+            .map(lambda x: (x, 1)) \
+            .reduceByKey(lambda x,y: 1) \
+            .map(lambda x: (x[0][1], 1)) \
+            .reduceByKey(lambda x,y: x+y) \
+            .top(25, key=lambda x: x[1]).saveAsTextFile(sys.argv[-1])
